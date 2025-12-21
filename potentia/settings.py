456aafc0c5c52e09e71732b36422d34e67e9ffa2
Jarlath <jarlath@jarlath.cc>
2025-12-21 00:03:22 -0600

    add env variables

--- potentia/settings.py
+++ potentia/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3v%+(k&3now^57xk(v)f(i#bu&mrd((^d(j-36gcn&=&byoru0'
+# SECRET_KEY = 'django-insecure-3v%+(k&3now^57xk(v)f(i#bu&mrd((^d(j-36gcn&=&byoru0'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
