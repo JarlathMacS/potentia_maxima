5747da47ea51715ca865c157e7bdcd509ceadb51
Jarlath <jarlath@jarlath.cc>
2026-01-02 22:36:23 -0600

    add css to static folder

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import CoachingPost, ProgressComment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import CoachingPost, ProgressComment
 
 
 @admin.register(CoachingPost)
