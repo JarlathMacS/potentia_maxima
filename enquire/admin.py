bca6dbe09e649c9228c352084e8a6e94b9770705
Jarlath <jarlath@jarlath.cc>
2026-01-20 21:21:04 -0600

    add created_on field to enquire model

--- enquire/admin.py
+++ enquire/admin.py
@@ -2,23 +2,12 @@ from django.contrib import admin
 from .models import FreeConsultationRequest
 
 
-# Register your models here.
-
-# Note: admin.ModelAdmin is the standard way of registering
-#       our model with the admin panel. We do it differently
-#       above because we are supplying Summernote fields.
-#       If you want to customise the admin panel view in your
-#       own projects, then inherit from admin.ModelAdmin like
-#       we do below.
-
-
 @admin.register(FreeConsultationRequest)
 class FreeConsultationRequestAdmin(admin.ModelAdmin):
     """
     Lists fields for display in admin, fields for search,
     field filters, and fields for ordering.
     """
-    list_display = ('message', 'read',)
-    list_filter = ('read',)
-    search_fields = ('message',)
-    ordering = ('-read',)
+    list_display = ('name', 'email', 'message', 'read', 'created_on',)
+    list_filter = ('read', 'created_on',)
+    search_fields = ('name', 'email', 'message',)
--- enquire/models.py
+++ enquire/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
 
 
 class FreeConsultationRequest(models.Model):
+    """
+    Stores a single free consultation request message
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
     read = models.BooleanField(default=False)
+    created_on = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["-read", "-created_on"]
 
     def __str__(self):
         return f"Free consultation request from {self.name}"
-
-        # max_length=200, unique=False, blank=False, null=False
