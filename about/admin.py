69b5f6e95b9d0f417c66cec072552f575856bd41
Jarlath <jarlath@jarlath.cc>
2026-01-20 21:32:47 -0600

    add list_filter field to about admin model

--- about/admin.py
+++ about/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import About
 
-# Register your models here.
-# admin.site.register(About, SummernoteModelAdmin)
-
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
@@ -15,3 +12,4 @@ class AboutAdmin(SummernoteModelAdmin):
     list_display = ('title', 'updated_on')
     search_fields = ['title', 'content']
     summernote_fields = ('content',)
+    list_filter = ('updated_on',)
--- about/models.py
+++ about/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
-
 
 class About(models.Model):
     """
-    Stores a single about me text
+    Stores a single about me post.
     """
     title = models.CharField(max_length=200)
     content = models.TextField()
